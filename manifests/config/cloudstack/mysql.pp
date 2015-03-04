affed37ba175007a01f4d2bb6d407aceef410e9f
Lavaburn <nicolas@truyens.com>
2015-03-04 18:46:07 +0100

    Patching schema setup. Bug in CS 4.4 ?

--- manifests/config/cloudstack/mysql.pp
+++ manifests/config/cloudstack/mysql.pp
@@ -25,9 +25,29 @@ class cloudstack::config::cloudstack::mysql inherits ::cloudstack {
   # TODO [FEATURE-REQUEST: Install from Source?]
   # => Can't depend on Package['cloudstack-management']
 
+
+  # TODO VERSION SPECIFIC - Currently a bug in cloudstack (4.4?) package..
+  $setup_dir = '/usr/share/cloudstack-management/setup'
+  $patch_name = 'cloudstack-schema-premium.patch'
+
+  file { "${setup_dir}/${patch_name}":
+    ensure => 'file',
+    source => "puppet:///modules/cloudstack/${patch_name}",
+  }
+
+  exec { 'patch-cloudstack-schema-premium':
+    command     => "/usr/bin/patch -p1 ${setup_dir}/create-schema-premium.sql < ${setup_dir}/${patch_name}",
+    cwd         => $setup_dir,
+    subscribe   => Package[$::cloudstack::cloudstack_mgmt_package_name],
+    refreshonly => true,
+    require     => File["${setup_dir}/${patch_name}"],
+  }
+
+
   exec { 'Setup Cloudstack with MySQL database':
     command     => "${bin} ${db} ${security} ${deploy}",
     subscribe   => Package[$::cloudstack::cloudstack_mgmt_package_name],
     refreshonly => true,
+    require     => Exec['patch-cloudstack-schema-premium'],
   }
 }
