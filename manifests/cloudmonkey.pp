db5d34f567f23dcd297e4fe921405d3d3fe31632
Lavaburn <nicolas@truyens.com>
2015-03-20 18:22:24 +0100

    Added CLoudMonkey

---
+++ manifests/cloudmonkey.pp
@@ -0,0 +1,17 @@
+# Class: cloudstack::cloudmonkey
+#
+# This will install the CloudMonkey CLI
+# You will need 'pip' installed - I did not include it here as a dependency.
+#
+class cloudstack::cloudmonkey inherits ::cloudstack {
+  # If you don't have 'pip' installed:
+#    puppet module install stankevich-python
+#    class { 'python' :
+#		   pip => true,
+#		 }
+
+	python::pip { 'cloudmonkey' :
+	  pkgname       => 'cloudmonkey',
+	  ensure        => 'latest',
+	 }
+}
